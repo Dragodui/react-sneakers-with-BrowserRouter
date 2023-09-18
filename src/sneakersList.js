4f6171aae2f552c42281cef5429496c5de6d920e
Aksandr Al-Ghazali <85372599+Dragodui@users.noreply.github.com>
2023-09-18 23:38:11 +0200

    idk

---
+++ src/sneakersList.js
@@ -0,0 +1,87 @@
+import sneaker1 from "./img/sneakers/1.jpg"
+import sneaker2 from "./img/sneakers/2.jpg"
+import sneaker3 from "./img/sneakers/3.jpg"
+import sneaker4 from "./img/sneakers/4.jpg"
+import sneaker5 from "./img/sneakers/5.jpg"
+import sneaker6 from "./img/sneakers/6.jpg"
+import sneaker7 from "./img/sneakers/7.jpg"
+import sneaker8 from "./img/sneakers/8.jpg"
+import sneaker9 from "./img/sneakers/9.jpg"
+import sneaker10 from "./img/sneakers/10.jpg"
+
+const sneakers = [
+    {
+        id:1,
+        title: 'Men\'s Nike Blazer Mid Suede Sneakers',
+        price:100,
+        img:sneaker1
+    },
+    {
+        id:2,
+        title: 'Men\'s Nike Air Max 270 Sneakers',
+        price:100,
+        img:sneaker2
+    },
+    {
+        id:3,
+        title: 'Puma X Aka Boku Future Rider sneakers.',
+        price:100,
+        img:sneaker3
+    },
+    {
+        id:4,
+        title: 'Men\'s Under Armour Curry 8 Sneakers',
+        price:100,
+        img:sneaker4
+    },
+    {
+        id:5,
+        title: 'Men\'s Nike Kyrie 7 Sneakers',
+        price:100,
+        img:sneaker5
+    },
+    {
+        id:6,
+        title: 'Men\'s Jordan Air Jordan 11 Sneakers',
+        price:100,
+        img:sneaker6
+    },
+    {
+        id:7,
+        title: 'Men\'s Nike LeBron XVIII Sneakers',
+        price:100,
+        img:sneaker7
+    },
+    {
+        id:8,
+        title: 'Men\'s Nike Lebron XVIII Low Sneakers',
+        price:100,
+        img:sneaker8
+    },
+    {
+        id:9,
+        title: 'Men\'s Nike Blazer Mid Suede Sneakers',
+        price:100,
+        img:sneaker9
+    },
+    {
+        id:10,
+        title: 'Puma X Aka Boku Future Rider sneakers.',
+        price:100,
+        img:sneaker10
+    },
+    {
+        id:11,
+        title: 'Puma X Aka Boku Future Rider sneakers.',
+        price:100,
+        img:sneaker1
+    },
+    {
+        id:12,
+        title: 'Puma X Aka Boku Future Rider sneakers.',
+        price:100,
+        img:sneaker10
+    },
+]
+
+export default sneakers;
